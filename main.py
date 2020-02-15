96f31e2bcfaa340039f0ae6a99de98071a57cea7
camer <CBieksha@student.bridgew.edu>
2020-02-15 18:48:44 -0500

    a

--- main.py
+++ main.py
@@ -4,125 +4,66 @@ import time
 import playsound
 import speech_recognition as sr
 from gtts import gTTS
-#creats image
-back = pygame.image.load("spath.png")
-playeri = pygame.image.load("testgal.png")
-monster = pygame.image.load("tsarm.png")
-pygame.display.set_icon(playeri)
-#Makes screen
-sc = pygame.display.set_mode((800,800))
-sc.blit(back,(0,0))
-# can be color of background
-#sc.fill((56,0,67))
-#sets background
-player = pygame.image
-playerx = 370
-playery = 400
-playerx_ch = 0
-#sc.blit(player,(playerx,playery))
-# makes player
-def play(x,y):
-    sc.blit(playeri,(x,y))
-#recounds voice
-def audio():
-# sets up mic
-    r = sr.Recognizer()
+from rp import *
+import pynput
+import fansi
+import numpy
+
+def audio_do():
+    import speech_recognition as sr
     with sr.Microphone() as source:
-        r.adjust_for_ambient_noise(source)
-        #r.adjust_for_ambient_noise(source)
-        audio = r.listen(source,timeout=1,phrase_time_limit=2)
-       # r.operation_timeout
-        said = ""
-       # r.operation_timeout
+        recognizer=sr.Recognizer()
+        recognizer.adjust_for_ambient_noise(source)
+        for _ in range(100) :
+            try:
+                audio=recognizer.listen(source,timeout=.25,phrase_time_limit=2)
+                text=recognizer.recognize_google(audio)
+                speech_text_handler(text)
+                fansi_print(text,'red')
+            except sr.WaitTimeoutError:
+                fansi_print('Timed out','red','bold')
+            except sr.UnknownValueError:
+                fansi_print('Didnt understand','red','bold')
+    return text
 
-    try:
-        said = r.recognize_google(audio)
-        print(str(said))
-    except Exception as e:
-        print("I did not understand that")
-    return said
+def speech_text_handler(text):
+    text=text.lower()
+    if 'north' in text:                     command_handler('north')
+    elif 'south' in text or 'self' in text: command_handler('south')
+    elif 'east' in text:                    command_handler('east')
+    elif 'wes' in text or 'west' in text:   command_handler('west')
+    elif 'stop' in text:                    command_handler('stop')
+    elif 'exit' in text:                    command_handler('exit')
+    else:
+        return
 
-#playerx += 56
-# makes so it dose not close
-run = True
-while run:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-    voice = audio()
-    print(voice)
-    # makes it go north
-    if "North" in voice:
-        hi = 1
-        #print(hi)
-        while hi == 1:
-            if playery <= 0:
-                playery = 0
-            else:
-                playery += -45
-            # updates and makes to go forward
-            sc.blit(back, (0, 0))
-            play(playerx, playery)
-            pygame.display.update()
-            #playery += -35
-            voice = audio()
-           # print(voice)
-            # stops going forward
-            if "stop" in voice:
-                hi = 0
-        # change y axes up
-        #playery += -35
-        print(voice)
-    if "West" in voice:
-        hi = 1
-        while hi == 1:
-        # change y axes left
-            if playerx <= 0:
-                playerx = 0
-            else:
-                playerx += -95
-            sc.blit(back, (0, 0))
-            play(playerx, playery)
-            pygame.display.update()
-            voice = audio()
-            if "stop" in voice:
-                hi = 0
 
-    if "East" in voice:
-        hi = 1
-        while hi == 1:
-        # change x axis right
-            if playerx >= 800:
-                playerx = 780
-            else:
-                playerx += 95
-            sc.blit(back, (0, 0))
-            play(playerx, playery)
-            pygame.display.update()
-            voice = audio()
-            if "stop" in voice:
-                hi = 0
-    if "South" in voice:
-        hi = 1
-        while hi == 1:
-        # change y axes down
-            if playery >= 800:
-                playery = 0
-            else:
-                playery += 45
-            sc.blit(back, (0, 0))
-            play(playerx, playery)
-            pygame.display.update()
-            voice = audio()
-            if "stop" in voice:
-                hi = 0
-    if "exit" in voice:
-        # this exits the game
-        run = False
-    #playerx_ch = -70.0
-    #sc.remove(playeri)
-    #playerx += 56
-    sc.blit(back,(0,0))
-    play(playerx,playery)
-    # chages ever thing
-    pygame.display.update()
+keyboard=pynput.keyboard.Controller()
+from time import sleep
+def press_key(key):
+    keyboard.press(key)
+    sleep(.05)
+    keyboard.release(key)
+
+last_command=''
+def command_handler(command):
+    #Don't do duplicate commands in a row
+    global last_command
+    if last_command==command:
+        return
+    last_command=command
+
+    print("COMMAND:",command)
+    text_to_speech("do "+command)
+
+    if   'north' == command:press_key("w")
+    elif 'south' == command:press_key("s")
+    elif 'east'  == command:press_key("d")
+    elif 'west'  == command:press_key("a")
+    elif 'stop'  == command:press_key("f")
+    elif 'exit'  == command:press_key("x")
+
+for _ in range(10):
+    run_as_new_thread(audio_do)
+    sleep(random_float())
+    print("STARTED",_)
